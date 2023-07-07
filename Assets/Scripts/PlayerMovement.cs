dd47037ac21af91024c7f6ad6fc05b3c6673d845
auspiciousArtifice <harrywang703@gmail.com>
2023-07-07 19:40:08 -0400

    Adds double jump capabilities

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,9 @@ public class Movement : MonoBehaviour
     //[SerializeField] private float jumpHeight;
     [SerializeField] private float jumpForce;
     [SerializeField] private float jumpTime;
+    [SerializeField] private int jumpCount;
     private float jumpTimeCounter;
+    private int jumpCounter;
     private bool grounded = false;
     private bool stoppedJumping;
     private Rigidbody2D body;
@@ -25,6 +27,8 @@ public class Movement : MonoBehaviour
     void Start()
     {
         jumpTimeCounter = jumpTime;
+        jumpCounter = jumpCount;
+        stoppedJumping = true;
     }
 
     // Update is called once per frame
@@ -43,36 +47,58 @@ public class Movement : MonoBehaviour
             transform.localScale = new Vector3(-4, 4, 1);
         }
 
-        if (grounded)
-        {
-            jumpTimeCounter = jumpTime;
-        }
-
         // Set animator params
         animator.SetBool("Running", horizontalInput != 0);
         animator.SetBool("Grounded", grounded);
+
+        if (grounded)
+        {
+            jumpCounter = jumpCount;
+            jumpTimeCounter = jumpTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && stoppedJumping && jumpCounter > 0)
+        {
+            stoppedJumping = false;
+            jumpTimeCounter = jumpTime;
+            jumpCounter -= grounded ? 0 : 1;
+            animator.SetTrigger("Jump");
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+        }
     }
 
     void FixedUpdate()
     {
-        // If spacebar is let go
-        if (!Input.GetKey(KeyCode.Space))
-        {
-            // Stop jumping and set jump counter to zero. Counter will reset once grounded.
-            jumpTimeCounter = 0;
-            stoppedJumping = true;
-        }
+        //// If spacebar is let go
+        //if (Input.GetKeyUp(KeyCode.Space))
+        //{
+        //    // Stop jumping and set jump counter to zero. Counter will reset once grounded.
+        //    jumpTimeCounter = 0;
+        //    stoppedJumping = true;
+        //}
 
-        // If pressing space and grounded
-        if (Input.GetKey(KeyCode.Space) && grounded)
-        {
-            body.velocity = new Vector2(body.velocity.x, jumpForce);
-            stoppedJumping = false;
-            animator.SetTrigger("Jump");
-        }
+        //// If pressing space and have jumps left
+        //if (Input.GetKeyDown(KeyCode.Space) && stoppedJumping && jumpCounter > 0)
+        //{
+        //    body.velocity = new Vector2(body.velocity.x, jumpForce);
+        //    stoppedJumping = false;
+        //    jumpTimeCounter = jumpTime;
+        //    jumpCounter--;
+        //    animator.SetTrigger("Jump");
+        //}
 
-        // If holding space while jumping and jumping time not yet reached
-        if (Input.GetKey(KeyCode.Space) && !stoppedJumping && jumpTimeCounter > 0) 
+        //// If holding space while jumping and jumping time not yet reached
+        //if (Input.GetKey(KeyCode.Space) && !stoppedJumping && jumpTimeCounter > 0) 
+        //{
+        //    body.velocity = new Vector2(body.velocity.x, jumpForce);
+        //    jumpTimeCounter -= Time.deltaTime;
+        //}
+
+        if (!stoppedJumping && jumpTimeCounter > 0)
         {
             body.velocity = new Vector2(body.velocity.x, jumpForce);
             jumpTimeCounter -= Time.deltaTime;
@@ -80,13 +106,6 @@ public class Movement : MonoBehaviour
 
         body.velocity = new Vector2(body.velocity.x, Mathf.Clamp(body.velocity.y, -maxFallSpeed, maxJumpSpeed));
     }
-
-    //private void Jump()
-    //{
-        
-    //    grounded = false;
-    //    animator.SetTrigger("Jump");
-    //}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
