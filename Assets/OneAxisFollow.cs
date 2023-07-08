a0962cc481127993c3f604c0ed40bc5227c6edd7
auspiciousArtifice <harrywang703@gmail.com>
2023-07-08 19:11:39 -0400

    Makes modifications to test level, and introduces tutorial level

---
+++ Assets/OneAxisFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneAxisFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject followObject;
+    [SerializeField] private Axis followAxis;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 objectPos = followObject.transform.position;
+        Vector3 setPosition = transform.position;
+        setPosition.x = followAxis == Axis.X ? objectPos.x : setPosition.x;
+        setPosition.y = followAxis == Axis.Y ? objectPos.y : setPosition.y;
+        transform.position = setPosition;
+    }
+}
+
+enum Axis
+{
+    X, Y, Z
+}
