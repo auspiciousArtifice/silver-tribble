49b1ed6fb1859478fe3e6f5331c09f4ebf77d24d
ColinZhong73 <legendsword73@gmail.com>
2023-07-08 12:38:46 -0500

    Basic sacrifice powerup door implemented. Disable double jumping is bugged and disables jumping in general.

--- Assets/Scripts/Block Scripts/WinZone.cs
+++ Assets/Scripts/Block Scripts/WinZone.cs
@@ -6,18 +6,6 @@ public class WinZone : MonoBehaviour
 {
     public GameObject winScreen;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Player") {
             Time.timeScale = 0;
