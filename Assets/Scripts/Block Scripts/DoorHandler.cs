f209dd01de6d40e6cbf0c5d3621fda3e5db52ffd
ColinZhong73 <legendsword73@gmail.com>
2023-07-08 20:23:01 -0500

    Door Prefabs + GameUI Prefabs (both WIP). Started LongLevel

--- Assets/Scripts/Block Scripts/DoorHandler.cs
+++ Assets/Scripts/Block Scripts/DoorHandler.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class DoorHandler : MonoBehaviour
 {
     public GameObject doorScreen;
-    public GameObject player;
     public GameObject door;
 
     public void openDoor(int i){
         if(i == 1){
-            player.GetComponent<Movement>().jumpCount--;
+            GameObject.FindWithTag("Player").GetComponent<Movement>().jumpCount--;
         }
         else if(i == 2){
-            player.GetComponent<Movement>().dashCount--;
+            GameObject.FindWithTag("Player").GetComponent<Movement>().dashCount--;
         }
         GameObject.FindWithTag("Respawn").transform.position = transform.position;
         doorScreen.SetActive(false);
         door.SetActive(false);
+        gameObject.SetActive(false);
         Time.timeScale = 1;
     }
 
